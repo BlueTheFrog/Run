da1dfb9b6232a265b43666ad45e80fa1ad4bfce1
BlueTheFrog <ochoad64@gmail.com>
2015-05-21 08:44:11 -0400

    Started game

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		GetComponent<Rigidbody> ().velocity = new Vector3 (GetComponent<Rigidbody> ().velocity.x, 
+		                                                   GetComponent<Rigidbody> ().velocity.y, 
+		                                                   speed * Time.deltaTime);	
+	}
+}
