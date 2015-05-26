f032bbe7262e433ed54308cc53329a4b3ab6d05f
BlueTheFrog <ochoad64@gmail.com>
2015-05-25 21:55:22 -0400

    Made more progress on movement and added texture

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,26 +7,43 @@ public class Movement : MonoBehaviour
 	public float turnSpeed;
 	public float jumpForce;
 	float forwardSpeedBackUp;
+    Vector3 startPos;
+    bool hasJumped = false;
 
 	// Use this for initialization
 	void Start ()
 	{
 		forwardSpeedBackUp = forwardSpeed;
+        startPos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+        // Puts the player back at the start if they fall off
+        if (transform.position.y < -40f)
+        {
+            transform.position = startPos;
+        }
 		// If the player is grounded and they press the jump button, jump
-		if (isGrounded() && Input.GetAxisRaw("Jump") == 1f)
+		if (Input.GetAxisRaw("Jump") == 1f)
 		{
-			GetComponent<Rigidbody>().AddForce(new Vector3(0f, jumpForce, 0f));
+            if (isGrounded() && !hasJumped)
+            {
+                GetComponent<Rigidbody>().AddForce(new Vector3(0f, jumpForce, 0f));
+            }
+            hasJumped = true;
 		}
+        else
+        {
+            hasJumped = false;
+        }
 
 		// Recieves left and right input from the player
 		float h = Input.GetAxisRaw ("Horizontal");
 
-		restrictMovement ();
+        // Makes sure the player doesn't get stuck when moving forward
+		//restrictMovement ();
 
 		// Perpetually moves forward and makes the player move left and right
 		GetComponent<Rigidbody> ().velocity = new Vector3 (h * Time.deltaTime * turnSpeed, 
@@ -37,7 +54,10 @@ public class Movement : MonoBehaviour
 	// Returns true if the player is on the ground. Duh
 	bool isGrounded ()
 	{
-		if (Physics.Raycast(transform.position, Vector3.down, GetComponent<BoxCollider>().size.y / 2 + 0.1f))
+        BoxCollider bc = GetComponent<BoxCollider>();
+		if (Physics.Raycast(transform.position, Vector3.down, bc.size.y / 2f + 0.1f) ||
+            Physics.Raycast(new Vector3(transform.position.x + bc.size.x / 2f, transform.position.y, transform.position.z), Vector3.down, bc.size.y / 2f + 0.1f) ||
+            Physics.Raycast(new Vector3(transform.position.x - bc.size.x / 2f, transform.position.y, transform.position.z), Vector3.down, bc.size.y / 2f + 0.1f))
 		{
 			return true;
 		}
@@ -47,26 +67,52 @@ public class Movement : MonoBehaviour
 	void restrictMovement ()
 	{
 		forwardSpeed = forwardSpeedBackUp;
+        BoxCollider bc = GetComponent<BoxCollider>();
 
-		// Center
-		if (Physics.Raycast(transform.position, Vector3.forward, GetComponent<BoxCollider>().size.z / 2 + 0.1f))
+		// Top Left
+		if (Physics.Raycast(new Vector3(transform.position.x - bc.size.x / 2f, transform.position.y + bc.size.y / 2f, transform.position.z), Vector3.forward, bc.size.z / 2 + 0.1f))
 		{
 			forwardSpeed = 0f;
 		}
-		// Top (Not Finished)
-		if (Physics.Raycast(transform.position, Vector3.forward, GetComponent<BoxCollider>().size.z / 2 + 0.1f))
+		// Top
+		if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y + bc.size.y / 2f, transform.position.z), Vector3.forward, GetComponent<BoxCollider>().size.z / 2 + 0.1f))
 		{
 			forwardSpeed = 0f;
 		}
-		// Top Left (Not Finished)
-		if (Physics.Raycast(transform.position, Vector3.forward, GetComponent<BoxCollider>().size.z / 2 + 0.1f))
+		// Top Right
+        if (Physics.Raycast(new Vector3(transform.position.x + bc.size.x / 2f, transform.position.y + bc.size.y / 2f, transform.position.z), Vector3.forward, GetComponent<BoxCollider>().size.z / 2 + 0.1f))
 		{
 			forwardSpeed = 0f;
 		}
-		// Top Right (Not Finished)
-		if (Physics.Raycast(transform.position, Vector3.forward, GetComponent<BoxCollider>().size.z / 2 + 0.1f))
+		// Left
+        if (Physics.Raycast(new Vector3(transform.position.x - bc.size.x / 2f, transform.position.y, transform.position.z), Vector3.forward, GetComponent<BoxCollider>().size.z / 2 + 0.1f))
 		{
 			forwardSpeed = 0f;
 		}
+        // Middle
+        if (Physics.Raycast(transform.position, Vector3.forward, GetComponent<BoxCollider>().size.z / 2 + 0.1f))
+        {
+            forwardSpeed = 0f;
+        }
+        // Right
+        if (Physics.Raycast(new Vector3(transform.position.x + bc.size.x / 2f, transform.position.y, transform.position.z), Vector3.forward, GetComponent<BoxCollider>().size.z / 2 + 0.1f))
+        {
+            forwardSpeed = 0f;
+        }
+        // Bottom Left
+        if (Physics.Raycast(new Vector3(transform.position.x - bc.size.x / 2f, transform.position.y - bc.size.y / 2f, transform.position.z), Vector3.forward, GetComponent<BoxCollider>().size.z / 2 + 0.1f))
+        {
+            forwardSpeed = 0f;
+        }
+        // Bottom
+        if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y - bc.size.y / 2f, transform.position.z), Vector3.forward, GetComponent<BoxCollider>().size.z / 2 + 0.1f))
+        {
+            forwardSpeed = 0f;
+        }
+        // Bottom Right
+        if (Physics.Raycast(new Vector3(transform.position.x + bc.size.x / 2f, transform.position.y - bc.size.y / 2f, transform.position.z), Vector3.forward, GetComponent<BoxCollider>().size.z / 2 + 0.1f))
+        {
+            forwardSpeed = 0f;
+        }
 	}
 }
