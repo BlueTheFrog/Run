81a36d129fdbcc766b2336e0c38f108e270eb1c7
BlueTheFrog <ochoad64@gmail.com>
2015-05-29 08:42:38 -0400

    Made spring functional and started level 2

---
+++ Assets/Scripts/Spring.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spring : MonoBehaviour
+{
+	public float bounceForce;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.transform.tag == "Player")
+		{
+			other.transform.position = new Vector3 (other.transform.position.x, transform.position.y + transform.lossyScale.y * 0.5f, transform.position.z);
+			other.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+			other.GetComponent<Rigidbody>().AddForce(0f, bounceForce, 0f);
+		}
+	}
+}
