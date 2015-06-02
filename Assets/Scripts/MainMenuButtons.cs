056c11037787cd42ab4929115bc8215cc9c299fe
BlueTheFrog <ochoad64@gmail.com>
2015-06-02 08:42:08 -0400

    Did more stuff on menus

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenuButtons : MonoBehaviour
+{
+	string buttonName;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void changeLevel ()
+	{
+		buttonName = GetComponentInChildren<Text> ().text;
+		Application.LoadLevel ((int.Parse(buttonName[6].ToString())));
+	}
+}
