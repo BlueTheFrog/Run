92fd23cb06163c32ad47846dba3cc99af4bc96e3
BlueTheFrog <ochoad64@gmail.com>
2015-06-08 21:44:42 -0400

    Minor Changes

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("ToMenu"))
+        {
+            Application.LoadLevel(0);
+        }
+	    if (CrossPlatformInputManager.GetButtonDown("ToNextLevel"))
+        {
+            Application.LoadLevel(int.Parse(Application.loadedLevel.ToString()) + 1);
+        }
+	}
+}
